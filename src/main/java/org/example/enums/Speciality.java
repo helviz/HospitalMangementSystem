a09d761ac939c6ce6b3e047cd1a07c1894fd8ee3
helviz <twomoelvisrodney@gmail.com>
2025-06-28 10:54:26 +0300

    feat(appointmentStatus, paymentStatus, role, speciality): add AppointmentStatus and PaymentStatus and Speciality

---
+++ src/main/java/org/example/enums/Speciality.java
@@ -0,0 +1,9 @@
+package org.example.enums;
+
+public enum Speciality {
+    GENERAL_MEDICINE,  // Common illnesses, primary care
+    PEDIATRICS,        // Children's health
+    GYNECOLOGY,        // Women's health and maternity
+    ORTHOPEDICS,       // Bone, joint, and muscle issues
+    DERMATOLOGY        // Skin-related issues
+}
