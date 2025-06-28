37ab2a1dfcc83cc343afc315f2bf1839c8e5daf1
helviz <twomoelvisrodney@gmail.com>
2025-06-28 11:03:34 +0300

    feat(UpdateCredentials): add UpdateCredentials

---
+++ src/main/java/org/example/controller/base/UpdateCredentials.java
@@ -0,0 +1,10 @@
+package org.example.controller.base;
+
+public interface UpdateCredentials {
+    private void updateFirstName() {}
+    private void updateMiddleName(){};
+    private void updateLastName(){};
+    private void updateEmail(){};
+    private void updateContactNumber(){};
+    private void updateDateOfBirth(){};
+}
