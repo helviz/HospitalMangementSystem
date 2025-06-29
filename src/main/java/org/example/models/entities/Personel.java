23e0f4fc7fc76f6d8659de6997a53323473c48aa
helviz <twomoelvisrodney@gmail.com>
2025-06-29 20:57:37 +0300

    bg-fix(personel, pom): changed Personel and pom.xml

--- src/main/java/org/example/models/entities/Personel.java
+++ src/main/java/org/example/models/entities/Personel.java
@@ -1,7 +1,5 @@
 package org.example.models.entities;
-
 import jakarta.persistence.*;
-
 import java.time.LocalDate;
 
 @MappedSuperclass
@@ -22,7 +20,7 @@ public class Personel {
     @Column(name="contact_number")
     private String contactNumber;
 
-    @Column(name="email")
+    @Column(name="email ")
     private String email;
 
 //    setters and getters
