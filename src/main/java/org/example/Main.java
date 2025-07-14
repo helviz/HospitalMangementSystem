0056d659461ae8032d95724d21f5d775b5b578ef
helviz <twomoelvisrodney@gmail.com>
2025-07-14 12:55:52 +0300

    updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,29 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.dao.AppointmentDAO;
+import org.example.services.Impl.MedicalRecordServiceImpl;
+import org.example.services.MedicalRecordService;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("System is running");
 
+        AppointmentDAO appointmentDAO = new AppointmentDAO();
+        Long patientId = Long.valueOf(68);
+        Long doctorId = Long.valueOf(1);
+        LocalDateTime  noon = LocalDateTime.of(2025, 7, 11, 12, 0);
+        LocalDateTime  afternoon = LocalDateTime.of(2025, 7, 11, 3, 0);
+        boolean success;
+        success = appointmentDAO.createAppointment(patientId, doctorId, afternoon);
+
+        if (success) System.out.println("Created an appointment at afternoon" );
+
+
+
 
 
     }
-
-
-
 }
